7acd38bf5414ccd68d324631678772a63dab6243
ValeriaGongar <valeria.gongar@gmail.com>
2022-07-30 20:59:13 -0500

    useEffect

--- menu-resto/src/App.js
+++ menu-resto/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import Footer from './components/Footer'
+import React, { useEffect } from 'react'
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 import Newsletter from './components/Newsletter'
@@ -8,10 +7,37 @@ import Products from './components/Products'
 import ScrollToTop from './components/ScrollToTop'
 import Services from './components/Services'
 import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+import scrollreveal from "scrollreveal"
 
 export default function App() {
+  useEffect(() => {
+    const sr=scrollreveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        nav,
+        #home,
+        #services,
+        #portfolio,
+        #testimonials,
+        #products,
+        #newsletter,
+        .footer
+    `,
+      {
+        opacity: 0,
+        interval: 200,
+      }
+    );
+  }, []);
   
-  return <>
+  return (
+  <>
   <NavBar/>
   <ScrollToTop/>
   <Hero/>
@@ -21,6 +47,6 @@ export default function App() {
   <Products/>
   <Newsletter/>
   <Footer/>
-  </>;
-  
+  </>
+  );
 }
